f7a958ef88d2ec685756cf1eed8c1749ba4d8495
Duc Nguyen <nguyeduc@oregonstate.edu>
2022-03-24 17:04:01 -0400

    Initial Push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Movies from "./Components/Movies";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
